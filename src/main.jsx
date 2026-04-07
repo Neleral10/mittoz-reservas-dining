c3547f9e285352a08fa1c7bf98f7723d3f95fcdf
Neleral10 <nelson.mittoz@gmail.com>
2026-04-07 16:01:32 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MittozReservas from './MittozReservas.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MittozReservas />
+  </React.StrictMode>
+);
